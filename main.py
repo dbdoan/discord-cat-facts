d3c7ef2e749bfbd2e0f8bf43f3744319ac66644d
“dbdoan” <40965142+dbdoan@users.noreply.github.com>
2024-08-20 21:19:12 -0500

    new main file

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-import requests
+import asyncio
+import datetime
 import discord
+from discord.ext import commands, tasks
 import math
+import dotenv
 import random
+import requests
 import pandas
-from os import system
-system('clear')
+import os
+import sys
 
 # /////////// /////////// ///////////
-try:
-    response = requests.get('https://cat-fact.herokuapp.com/facts')
-    response.raise_for_status()
-    if response.ok:
-        print(f"Status Code: {response.status_code}")
-except requests.exceptions.HTTPError as http_err:
-    print(f"HTTP Error occurred: {http_err}")
-except Exception as err:
-    print(f"Error: {err}")
+os.system('clear')
 
-# /////////// /////////// ///////////
-cat_facts_df = pandas.DataFrame(data=response.json())
-random_fact_val = random.randint(0, len(cat_facts_df))
-print(cat_facts_df['text'][1])
+dotenv.load_dotenv()
+DISCORD_BOT_TOKEN = os.getenv('BOT_TOKEN')
+if not DISCORD_BOT_TOKEN:
+    print("No bot token has been set.")
+    sys.exit(1)
+else:
+    print("Bot token has been set.")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Load the Cog
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+async def load_cogs():
+    await bot.load_extension("message_scheduler")
+
+async def main():
+    await load_cogs()
+    await bot.start(DISCORD_BOT_TOKEN)
+
+# Run the bot using asyncio.run()
+if __name__ == "__main__":
+    asyncio.run(main())
